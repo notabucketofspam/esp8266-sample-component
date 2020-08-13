fb2312aabbecaf3233b009884af9098655540ac7
notabucketofspam <not.a.bucket.of.spam@gmail.com>
2020-08-13 15:47:44 -0400

    Initial commit

---
+++ include/simple_sample.h
@@ -0,0 +1,16 @@
+#ifndef SIMPLE_SAMPLE_H
+#define SIMPLE_SAMPLE_H
+
+#include <stdio.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void printf_sample(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // SIMPLE_SAMPLE_H
---
+++ sample_component/simple_sample.c
@@ -0,0 +1,5 @@
+#include "simple_sample.h"
+
+void printf_sample(void){ 
+  printf("This is a simple sample");
+}
